066049f678ba6d2b614d9eea638ca9e7be938e1d
h1kupz <h1kupz3z@gmail.com>
2022-06-17 20:53:34 +0700

    Add @ to Bg Image

--- layouts/fullpage.vue
+++ layouts/fullpage.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-[url('HomeBG.jpg')] bg-cover bg-left w-screen mx-auto h-screen"
+    class="bg-[url('@/HomeBG.jpg')] bg-cover bg-left w-screen mx-auto h-screen"
   >
     <div class="relative font-body flex flex-col h-screen">
       <HeaderSection />
