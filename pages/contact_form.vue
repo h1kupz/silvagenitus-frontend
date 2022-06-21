745afd6e55035cc37fa051de81e8711696989bcc
h1kupz <h1kupz3z@gmail.com>
2022-06-21 22:25:00 +0700

    Static Form

--- pages/contact_form.vue
+++ pages/contact_form.vue
@@ -4,24 +4,29 @@
       class="bg-hover w-screen max-w-xl px-4 py-6 pt-8 mx-4 rounded-xl bg-opacity-70"
     >
       <form
-        action="https://fabform.io/f/-rm95fD"
+        action="https://api.staticforms.xyz/submit"
         method="post"
         class="flex flex-col"
       >
+        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="email" placeholder="Email" />
+        <textarea name="message" placeholder="Message"></textarea>
+        <input type="text" name="honeypot" style="display: none" />
         <input
-          name="name"
-          type="text"
-          placeholder="Name"
-          required
-          class="rounded-t-lg"
+          type="hidden"
+          name="accessKey"
+          value="46b1bac2-86a9-4669-a0a8-e0ec573d7050"
         />
-        <input name="email" type="email" placeholder="Email" required />
-        <textarea
-          name="message"
-          type="text"
-          placeholder="Message"
-          required
-          class="rounded-b-lg"
+        <input
+          type="hidden"
+          name="subject"
+          value="Contact us from - example.com"
+        />
+        <input type="hidden" name="replyTo" value="@" />
+        <input
+          type="hidden"
+          name="redirectTo"
+          value="https://example.com/contact/success"
         />
         <button class="orderButton self-center" type="submit">
           <h4 class="m-0 self-center">Submit</h4>
