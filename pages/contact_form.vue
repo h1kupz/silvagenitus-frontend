44571d2063fb0e4ae1096499b1a7ab9f47145110
h1kupz <h1kupz3z@gmail.com>
2022-06-21 22:07:36 +0700

    FabForm Style

--- pages/contact_form.vue
+++ pages/contact_form.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="flex items-center justify-center h-screen">
-    <form
-      action="https://fabform.io/f/-rm95fD"
-      method="post"
-      class="flex flex-col"
+    <div
+      class="bg-hover w-screen max-w-xl px-4 py-6 pt-8 mx-4 rounded-xl bg-opacity-70"
     >
-      <input name="name" type="text" placeholder="Name" required />
-      <input name="email" type="email" placeholder="Email" required />
-      <textarea name="message" type="text" placeholder="Message" required />
-      <button class="orderButton" type="submit">
-        <h4 class="m-0 self-center">Submit</h4>
-      </button>
-    </form>
+      <form
+        action="https://fabform.io/f/-rm95fD"
+        method="post"
+        class="flex flex-col"
+      >
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          required
+          class="rounded-t-lg"
+        />
+        <input name="email" type="email" placeholder="Email" required />
+        <textarea
+          name="message"
+          type="text"
+          placeholder="Message"
+          required
+          class="rounded-b-lg"
+        />
+        <button class="orderButton self-center" type="submit">
+          <h4 class="m-0 self-center">Submit</h4>
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
