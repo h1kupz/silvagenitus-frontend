a9f339c568da1b3ceb56afb124247f98657aab5e
h1kupz <h1kupz3z@gmail.com>
2022-06-21 21:19:56 +0700

    FabForm Test

--- pages/contact_form.vue
+++ pages/contact_form.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex items-center justify-center h-screen">
-    <ContactForm />
+    <form action="https://fabform.io/f/-rm95fD" method="post">
+      <label for="email">Your Email</label>
+      <input name="email" type="email" />
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
