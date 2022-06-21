1a2f3d6ec4fba987566f497055a65cf3444ef206
h1kupz <h1kupz3z@gmail.com>
2022-06-21 18:57:05 +0700

    404 error

---
+++ layouts/error.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="relative font-body flex flex-col h-screen">
+    <HeaderSection />
+    <section class="flex h-max my-auto">
+      <slot />
+    </section>
+  </div>
+</template>
---
+++ pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="max-w-screen-md mx-auto">
+    <h2 class="text-hover text-center">Whoops!</h2>
+    <h2 class="text-center">The page you are looking for doesn't exist.</h2>
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  layout: "error",
+});
+</script>
